ec2f87ce8437073df84664671035c43651a96392
Cheton Wu <cheton@gmail.com>
2018-08-04 07:48:19 +0800

    Create AsyncAction.js

---
+++ src/AsyncAction.js
@@ -0,0 +1,25 @@
+class AsyncAction {
+    constructor() {
+        this.pending = true;
+        this.fulfilled = false;
+        this.rjected = false;
+
+        this.promise = new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+
+        this.promise
+            .then(v => {
+                this.pending = false;
+                this.fulfilled = true;
+                return v;
+            })
+            .catch(e => {
+                this.pending = false;
+                this.rejected = true;
+            });
+    }
+}
+
+export default AsyncAction;
